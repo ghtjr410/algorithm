6d7ef2510e5454c3e65587d5318e50bcbc0373c4
ghtjr410 <ghtjr410@gmail.com>
2025-12-28 12:26:28 +0900

    solve: boj_2920

---
+++ src/s1_implementation/boj_2920.java
@@ -0,0 +1,32 @@
+package s1_implementation;
+
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        String[] arr = line.split(" ");
+
+        boolean ascending = true;
+        boolean descending = true;
+        for (int i = 0; i < 8; i++) {
+            int num = Integer.parseInt(arr[i]);
+            if (i + 1 != num) ascending = false;
+            if (8 - i != num) descending = false;
+        }
+
+        String result = "mixed";
+
+        if (ascending) {
+            result = "ascending";
+        }
+
+        if (descending) {
+            result = "descending";
+        }
+
+        System.out.println(result);
+    }
+}
