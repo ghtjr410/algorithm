0d80d3209d09ae9c7ac94dd7d1d17e27f556f05c
ghtjr410 <ghtjr410@gmail.com>
2026-01-05 19:08:34 +0900

    solve: Boj15650

---
+++ src/s4_bruteforce/Boj15650.java
@@ -0,0 +1,42 @@
+package s4_bruteforce;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj15650 {
+    static int n, m;
+    static int[] selected;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        selected = new int[m];
+
+        pick(1, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    static void pick(int start, int depth) {
+        if (depth == m) {
+            for (int num : selected) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            selected[depth] = i;
+            pick(i + 1, depth + 1);
+        }
+    }
+}
+
